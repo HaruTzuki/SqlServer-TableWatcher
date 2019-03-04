7fe16a403396395b65ec52665a66d489d9569a45
Dimitris Vlachopoulos <dimitris.v.91@hotmail.com>
2019-03-04 23:43:58 +0200

    Commit to git
    Commit to git

---
+++ libwatcher_mssql/libwatcher_mssql/Watcher.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace libwatcher_mssql
+{
+    public class Watcher
+    {
+        private readonly string ConnectionString;
+        private readonly SqlConnection Connection;
+        private long TableCheckSumTemp;
+        
+
+        //Public Properties
+        public string TableName;
+        public long TableCheckSum;
+
+        //Events
+        public delegate void OnDbSchemaChanged();
+        public event OnDbSchemaChanged OnDbSchema_Changed;
+
+
+        #region Constructors
+        public Watcher()
+        {
+            this.ConnectionString = string.Empty;
+            this.Connection = null;
+            this.TableName = string.Empty;
+        }
+
+        public Watcher(string TableName)
+        {
+            this.TableName = TableName;
+        }
+
+        public Watcher(string ConnectionString, string TableName)
+        {
+            this.ConnectionString = ConnectionString;
+            this.Connection = new SqlConnection(this.ConnectionString);
+            this.TableName = TableName;
+        }
+
+        public Watcher(SqlConnection Connection, string TableName)
+        {
+            this.Connection = Connection;
+            this.TableName = TableName;
+        }
+
+        public Watcher(string Hostname, string UserId, string Password, string Database, string TableName)
+        {
+            this.ConnectionString = $"Integrated Security=false;Pooling=false;User ID={UserId};Password={Password};Data Source={Hostname};Initial Catalog={Database}";
+            this.Connection = new SqlConnection(this.ConnectionString);
+            this.TableName = TableName;
+        } 
+        #endregion
+
+        /// <summary>
+        /// Async method that start to watch a table.
+        /// </summary>
+        /// <returns></returns>
+        public async Task StartWatchAsync()
+        {
+            if (string.IsNullOrEmpty(this.TableName))
+                throw new Exception("One or more property not defined");
+
+            string Query = $"SELECT cast(CHECKSUM_AGG(BINARY_CHECKSUM(*)) as bigint) as Checksums FROM {this.TableName} WITH (NOLOCK);";
+
+            while (true)
+            {
+                try
+                {
+                    if (this.Connection.State != ConnectionState.Open)
+                        await this.Connection.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand(Query, this.Connection))
+                    {
+                        SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                        while (await reader.ReadAsync())
+                        {
+                            this.TableCheckSumTemp = await reader.IsDBNullAsync(reader.GetOrdinal("Checksums")) ? 0 : reader.GetInt64(reader.GetOrdinal("Checksums"));
+                        }
+
+                        if (this.TableCheckSum != this.TableCheckSumTemp)
+                        {
+                            this.TableCheckSum = this.TableCheckSumTemp;
+                            OnDbSchema_Changed?.Invoke();
+                        }
+
+                        reader?.Close();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+
+            }
+        }
+        
+    }
+}
---
+++ libwatcher_mssql/libwatcher_mssql_sample/Program.cs
@@ -0,0 +1,83 @@
+﻿using libwatcher_mssql;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace libwatcher_mssql_sample
+{
+    class Program
+    {
+        static private List<string> DataList;
+        static private string TableName;
+        static private Watcher watcher;
+
+        static void Main(string[] args)
+        {
+
+            //Descriptions
+            WriteLine("This is a Watcher Sample");
+            WriteLine("//====//");
+
+            //Reading from User table Name
+            TableName = ReadLine();
+
+            //Print Table Name
+            WriteLine($"The table is: {TableName}");
+
+            //Initialize Objects
+            InitData();
+
+            StartWatch();
+            
+            //Wait until user click a button
+            ReadKey();
+
+        }
+
+        private async static void StartWatch()
+        {
+            await watcher.StartWatchAsync();
+        }
+
+        private static void InitData()
+        {
+            DataList = new List<string>();
+            watcher = new Watcher("(local)", "sa", "1234", "TestDB", TableName.Trim());
+            watcher.OnDbSchema_Changed += Watcher_OnDbSchema_Changed;
+        }
+
+        private static void Watcher_OnDbSchema_Changed()
+        {
+            DataList.Clear();
+            string ConString = $"Integrated Security=false;Pooling=false;User ID=sa;Password=1234;Data Source=(local);Initial Catalog=TestDB";
+
+            using (SqlConnection conn = new SqlConnection(ConString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand($"Select Name from {TableName.Trim()}", conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        DataList.Add(reader.IsDBNull(reader.GetOrdinal("Name")) ? "" : reader.GetString(reader.GetOrdinal("Name")));
+                    }
+
+                    reader?.Close();
+                }
+            }
+
+            PrintData();
+        }
+
+        private static void PrintData()
+        {
+            foreach(var StrObj in DataList)
+            {
+                WriteLine(StrObj);
+            }
+
+            WriteLine("//====//");
+
+        }
+    }
+}
